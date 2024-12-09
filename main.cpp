a4b7428b771d249d7d17407c6ed57574b98b3e0d
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-12-09 18:02:58 +0200

    Added the Up and Down methods

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ using std::cin;
 using std::random_device;
 using std::mt19937;
 using std::uniform_int_distribution;
+using std::setw;
+
 
 void gotoxy(int x, int y) {
     COORD pos = { x, y };
@@ -38,16 +40,16 @@ void display_menu(int selected) {
 
     cout << "#>-------<MENU>-------<#" << endl;
     cout << "|                      |" << endl;
-    cout << "|        TOP           |" << endl;
-    cout << "|        RIGHT         |" << endl;
-    cout << "|        BOTTOM        |" << endl;
-    cout << "|        RIGHT         |" << endl;
-    cout << "|        Exit          |" << endl;
+    cout << "|         UP           |" << endl;
+    cout << "|         RIGHT        |" << endl;
+    cout << "|         DOWN         |" << endl;
+    cout << "|         LEFT         |" << endl;
+    cout << "|         EXIT         |" << endl;
     cout << "|                      |" << endl;
     cout << "#>--------------------<#" << endl;
 
-    for (int i = 1; i <= 5; ++i) {
-        gotoxy(9, y_coord);
+    for (int i = 1; i <= 5; i++) {
+        gotoxy(10, y_coord);
 
         if (i == selected) {
             set_background_color(0x0A);
@@ -57,11 +59,11 @@ void display_menu(int selected) {
         }
 
         switch (i) {
-        case 1: cout << "TOP"; break;
-        case 2: cout << "RIGHT"; break;
-        case 3: cout << "BOTTOM"; break;
-        case 4: cout << "RIGHT"; break;
-        case 5: cout << "Exit"; break;
+            case 1: cout << "UP"; break;
+            case 2: cout << "RIGHT"; break;
+            case 3: cout << "DOWN"; break;
+            case 4: cout << "LEFT"; break;
+            case 5: cout << "EXIT"; break;
         }
 
         y_coord++;
@@ -70,15 +72,82 @@ void display_menu(int selected) {
     setcolor(7);
 }
 
+
+int _output(int arr[][6], int size_x, int size_y)
+{
+    for (int i = 0; i < size_y; i++)
+    {
+        for (int j = 0; j < size_x; j++)
+        {
+            cout << setw(2) << arr[i][j];
+
+            cout << ' ';
+        }
+
+        cout << endl;
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
+int _up(int arr[][6], int size_x, int size_y, int number_of_shifts)
+{
+    for (int shifts = 0; shifts < number_of_shifts; shifts++) {
+        for (int j = 0; j < size_x; j++) {
+            int temp = arr[0][j];
+
+            for (int i = 0; i < size_y - 1; i++) {
+                arr[i][j] = arr[i + 1][j];
+            }
+            
+            arr[size_y - 1][j] = temp;
+        }
+    }
+
+    return 0;
+}
+
+int _down(int arr[][6], int size_x, int size_y, int number_of_shifts)
+{
+    for (int shifts = 0; shifts < number_of_shifts; shifts++) {
+        for (int j = 0; j < size_x; j++) {
+            int temp = arr[size_y - 1][j];
+
+            for (int i = size_y - 1; i > 0; i--) {
+                arr[i][j] = arr[i - 1][j];
+            }
+
+            arr[0][j] = temp;
+        }
+    }
+
+    return 0;
+}
+
+int _left(int arr[][6], int size_x, int size_y, int number_of_shifts)
+{
+    return 0;
+}
+
+int _right(int arr[][6], int size_x, int size_y, int number_of_shifts)
+{
+    return 0;
+}
+
+
 int main()
 {
 	const int size_x = 6;
 	const int size_y = 4;
 	int arr[size_y][size_x];
 
+    int number_of_shifts;
+
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<> dist(0, 9);
+	uniform_int_distribution<> dist(0, 50);
 
 	for (int i = 0; i < size_y; i++)
 	{
@@ -86,18 +155,6 @@ int main()
 		{
 			arr[i][j] = dist(gen);
 		}
-	}
-
-	for (int i = 0; i < size_y; i++)
-	{
-		for (int j = 0; j < size_x; j++)
-		{
-			cout << arr[i][j];
-
-			cout << ' ';
-		}
-
-		cout << endl;
 	}
 
     int selected = 1;
@@ -124,13 +181,27 @@ int main()
         else if (choice == 13) {
             system("cls");
 
+            if (selected != 5)
+            {
+                cout << "Enter the number of shifts: ";
+                cin >> number_of_shifts;
 
+                cout << endl << endl << "Before: " << endl;
+                _output(arr, size_x, size_y);
+            }
 
             switch (selected) {
-                case 5:
-                {
-                    return 0;
-                } break;
+                case 1: _up(arr, size_x, size_y, number_of_shifts); break;
+                case 2: _right(arr, size_x, size_y, number_of_shifts); break;
+                case 3: _down(arr, size_x, size_y, number_of_shifts);  break;
+                case 4: _left(arr, size_x, size_y, number_of_shifts); break;
+                case 5: return 0; break;
+            }
+
+            if (selected != 5)
+            {
+                cout << "After: " << endl;
+                _output(arr, size_x, size_y);
             }
 
             cout << endl << endl;
